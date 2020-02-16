a3012ead5739e9a68f0d41170bb79ad58a7f3eeb
郑李萍 <liping.zheng@huilianyi.com>
2020-02-16 17:38:23 +0800

    fix

--- src/main/java/com/practise/dataStructure/list/ListPractise.java
+++ src/main/java/com/practise/dataStructure/list/ListPractise.java
@@ -49,7 +49,7 @@ public class ListPractise {
 
     }
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
 //        forTest();
 //        forVector();
         forEnsureCapacity();
